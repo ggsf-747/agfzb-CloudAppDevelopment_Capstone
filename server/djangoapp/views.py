93a00b84b999934e857473b83c89c35527b6548e
ggsf-747 <ggsfermanis@gmail.com>
2023-03-22 21:13:20 -0400

    Final Changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -45,8 +45,28 @@ def logout_request(request):
     return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    if request.method == "GET":
+        return render(request, 'djangoapp/registration.html')
+    elif request.method == "POST":
+        username = request.POST['username1']
+        password = request.POST['password1']
+        first_name = request.POST['firstname1']
+        last_name = request.POST['lastname1']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name = first_name,
+            last_name = last_name, password=password)
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            context['message'] = "User has already been made"
+            return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
